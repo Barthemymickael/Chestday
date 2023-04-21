22f0c7b81980b32a1072a80f349ae85820178528
Alban <alban@MacBook-Pro-de-Alban.local>
2023-04-21 12:20:28 +0200

    ajout de la webSocket

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
 import { useState } from 'react'
-
+import { io } from "socket.io-client";
 import './App.css'
 // @ts-ignore
 import Chessboard from "chessboardjsx";
 
 function App() {
   const [count, setCount] = useState(0)
+  const [position, setPosition] = useState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+
+
+
+  const socket = io('http://localhost:8080', {
+    transports: ['websocket']
+  })
+
+  socket.on('onChange',(event)=>{setPosition(event)})
+
+  function handleDrop(e: any) {
+
+
+    socket.emit('game',JSON.stringify({"fen": position, e}))
+
+
+
+    // fetch('http://localhost:8080/game/test', requestOptions)
+    //     .then(response => response.json())
+    //     .then(res => setPosition(res))
+    // console.log(e);
+  }
+
+
 
   return (
     <div className="App">
-      <Chessboard position={"start"}></Chessboard>
+        <Chessboard position={position} onDrop={handleDrop}/>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import Register from "./register/register";
+import Login from "./login/login";
+import ForgotPassword from "./login/forgotPassword";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
